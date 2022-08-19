d5b2c0e52c2cbb4339031102ee591300a83734e5
lestercoyoy <lester_cch@hotmail.com>
2022-08-19 11:31:21 -0600

    ADD
    Mysql connection

---
+++ config/conexion.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'apirest_db'
+})
+
+conexion.connect((err) => {
+    if(err){
+        console.log('Error DB: ', err);
+        return err;
+    }
+    console.log('Conexion establecida!');
+})
